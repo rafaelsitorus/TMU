9dad306ddf24e9ceb9e50a2a8369b31ef01878f0
raja <rafaelraja.sitorus@gmail.com>
2026-03-10 21:02:13 +0700

    fix: deployment paging

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+import { resolve } from 'path'
+
 export default {
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        product: resolve(__dirname, 'product.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
   },
 }

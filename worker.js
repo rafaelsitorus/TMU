a49aa6299defe5cfcfe149b06e7f8e740ed7a392
rafaelsitorus <rafaelraja.sitorus@gmail.com>
2026-03-29 00:41:24 +0700

    feat: test v.1

--- worker.js
+++ worker.js
@@ -1,8 +1,9 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.hostname === 'www.todomitrautama.com') {
-      url.hostname = 'todomitrautama.com';
+    const host = request.headers.get('host') || '';
+    if (host.startsWith('www.')) {
+      const url = new URL(request.url);
+      url.hostname = host.slice(4);
       return Response.redirect(url.toString(), 301);
     }
     return env.ASSETS.fetch(request);
